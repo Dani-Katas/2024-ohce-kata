e002cfc7ed1ca62ab3f74d25119ea3527c0345b5
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-15 11:26:34 +0200

    meterlo dentro de una clase

--- src/main.js
+++ src/main.js
@@ -1,31 +1,37 @@
 import { createInterface } from 'node:readline/promises'
 import { stdin, stdout } from 'node:process'
 
-const rl = createInterface({ input: stdin, output: stdout })
+class Ohce {
+  async run() {
+    const rl = createInterface({ input: stdin, output: stdout })
 
-const name = process.argv[2]
-console.log(`¡Buenos días ${name}!`)
+    const name = process.argv[2]
+    console.log(`¡Buenos días ${name}!`)
 
-/**
- * @type string
- */
-let word = ""
+    /**
+     * @type string
+     */
+    let word = ""
 
 
-while(word !== "Stop!") {
-  word = await rl.question("")
+    while(word !== "Stop!") {
+      word = await rl.question("")
 
-  if (word === "Stop!") {
-    break
-  }
+      if (word === "Stop!") {
+        break
+      }
 
-  const reversed = word.split("").reverse().join("")
+      const reversed = word.split("").reverse().join("")
 
-  console.log(reversed)
+      console.log(reversed)
 
-  if (reversed === word) {
-    console.log("¡Bonita palabra!")
+      if (reversed === word) {
+        console.log("¡Bonita palabra!")
+      }
+    }
+
+    rl.close()
   }
 }
 
-rl.close()
+new Ohce().run()
