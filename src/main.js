b941e36ad3f87d79764364ec63e72b54733c424f
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-15 11:24:51 +0200

    almost implement ohce

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
-export function myFunction() {
-  return true
+import { createInterface } from 'node:readline/promises'
+import { stdin, stdout } from 'node:process'
+
+const rl = createInterface({ input: stdin, output: stdout })
+
+const name = process.argv[2]
+console.log(`¡Buenos días ${name}!`)
+
+/**
+ * @type string
+ */
+let word = ""
+
+
+while(word !== "Stop!") {
+  word = await rl.question("")
+
+  if (word === "Stop!") {
+    break
+  }
+
+  const reversed = word.split("").reverse().join("")
+
+  console.log(reversed)
+
+  if (reversed === word) {
+    console.log("¡Bonita palabra!")
+  }
 }
+
+rl.close()
